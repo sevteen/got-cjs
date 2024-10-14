d4f8064f5894a9c9ce0f238eadf7382051add977
sevteen <ilhamhanif07@gmail.com>
2024-10-14 20:10:01 +0700

    fix(build): trailing comma

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env zx 
-import { $, chalk } from 'zx' 
+import { $, chalk } from 'zx';
  
 try { 
-    await $`rollup -c` 
-    await $`echo '{"type": "module"}' > dist/esm/package.json` 
-    await $`echo '{"type": "commonjs"}' > dist/cjs/package.json`  
-    console.log(chalk.green('Compilation successful')) 
+    await $`rollup -c`;
+    await $`echo '{"type": "module"}' > dist/esm/package.json`;
+    await $`echo '{"type": "commonjs"}' > dist/cjs/package.json`;
+    console.log(chalk.green('Compilation successful'));
 } catch (error) { 
-    console.error(chalk.red('Compilation failed:'), chalk.red(error.message)) 
-} 
+    console.error(chalk.red('Compilation failed:'), chalk.red(error.message));
+};
